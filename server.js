e27ae70d7598b6d0321c4ad91a83b32fa5ee639b
Arcgo <imingjun.ma@outlook.com>
2023-02-12 21:57:38 -0800

    create memory and set port

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Keep the medical bills in memory
+let medicalBills = [];
